b4595e3423e0f584ad910b6b942dd51a017df193
Matvienko1710 <matvienkovitalii00@gmail.com>
2025-08-01 21:47:05 +0200

    Update db.js

--- db.js
+++ db.js
@@ -1,15 +1,14 @@
 const Database = require('better-sqlite3');
-
-const db = new Database('/data/users.db');
+const db = new Database('database.db');
 
 db.prepare(`
   CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY,
     username TEXT,
     stars INTEGER DEFAULT 0,
-    referred_by INTEGER,
     last_farm INTEGER DEFAULT 0,
-    last_bonus TEXT
+    last_bonus TEXT DEFAULT NULL,
+    referred_by INTEGER
   )
 `).run();
 
